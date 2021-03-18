8907acd1ef09c2a8a66abb6321b763e1546e13db
successs404 <neoruien@u.nus.edu>
2021-03-18 11:14:25 +0800

    Add components in ReadRecipe

--- src/pages/ReadRecipePage.js
+++ src/pages/ReadRecipePage.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import {
     SafeAreaView, 
-    View, 
-    Text
+    View,
+    Text,
+    ScrollView,
+    Image
 } from 'react-native';
+import styles from '../styles';
 
 function ReadRecipePage({route, navigation}) {
     const recipe = route.params;
     return (
         <SafeAreaView>
             <View>
-                <Text>Title: {recipe.title}</Text>
+                <ScrollView>
+                    <Image source={{uri: recipe.image}} style={styles.image}/>
+                    <Text style={styles.recipeTitle}>{recipe.title}</Text>
+                    <View style={{justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', flex: 1}}>
+                        <Text style={styles.recipeSubtitle}>{recipe.duration}</Text>
+                        <Text style={styles.recipeSubtitle}>{recipe.difficulty}</Text>
+                        <Text style={styles.recipeSubtitle}>{recipe.quantity} people</Text>
+                    </View>
+                    <Text style={styles.recipeTitle}>Ingredients</Text>
+                    {recipe.ingredients.map(ingredient => {
+                        <Text>ingredient</Text>
+                    })}
+                </ScrollView>
             </View>
         </SafeAreaView>
     )
